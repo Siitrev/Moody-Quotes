7046e98df7454f12bb51144cb96beab6748b6380
Przemyslaw Plata <paplata50@gmail.com>
2022-11-25 17:13:48 +0100

    commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 import json
 from datetime import datetime
 import logging
+from hashlib import sha256
 
 logging.basicConfig(level=logging.DEBUG,format='%(name)s - %(levelname)s - %(message)s')
 Builder.load_file("design.kv")
@@ -12,6 +13,19 @@ Builder.load_file("design.kv")
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current =  "sign_up_screen"
+        
+    def login(self,uname:str,pword:str):
+        print()
+        with open("users.json") as file:
+            try:
+                users = json.load(file)
+            except json.decoder.JSONDecodeError:
+                users = {}
+                
+        if uname in users and users[uname]["password"] == sha256(pword.encode()).hexdigest():
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.wrong_login.text = "Wrong username or password. Try again"
     pass
 
 class SignUpScreen(Screen):
@@ -24,24 +38,38 @@ class SignUpScreen(Screen):
             except json.decoder.JSONDecodeError:
                 users = {}
             
-        for v in users.values():
-            if v["name"] == uname:
+        for v in users:
+            if uname in v:
                 u_exist = 0
                 break
-        if u_exist and len(uname):users.setdefault("user"+str(len(users)+1),
-                        {"name":uname,
-                         "password":hash(pword),
-                         "created": datetime.now().strftime("%d-%m-%Y %H:%M")})
+        if u_exist and len(uname):
+            users.setdefault(uname,
+            {"name":uname,
+            "password":sha256(pword.encode()).hexdigest(),
+            "created": datetime.now().strftime("%d-%m-%Y %H:%M")})
+            self.manager.current = "sign_up_screen_success"
         else:
             logging.critical("DANY UZYTKOWNIK ISTNIEJE")
         with open("users.json","w") as file:
             json.dump(users,file)
         
-
         
+class SignUpScreenSuccess(Screen):
+    def go_back(self):
+        self.manager.transition.direction = "right"
+        self.manager.current =  "login_screen"
+    pass
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+    pass
 
 class RootWidget(ScreenManager):
     pass
+
+
 
 class MainApp(App):
     def build(self):
