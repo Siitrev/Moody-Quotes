77d97301e4301c0d889244611e2a38e9598ef09a
Przemyslaw Plata <paplata50@gmail.com>
2022-11-26 20:19:13 +0100

    commit

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.app import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
 from datetime import datetime
-import logging
 from hashlib import sha256
-from random import randint
+from random import choice
 from os import listdir
+from hoverable import HoverBehavior
+import logging, json
+
 
 logging.basicConfig(level=logging.DEBUG,format='%(name)s - %(levelname)s - %(message)s')
 Builder.load_file("design.kv")
@@ -33,7 +36,7 @@ class LoginScreen(Screen):
             self.ids.wrong_login.text = "Wrong username or password. Try again"
 
 class SignUpScreen(Screen):
-    def add_user(self, uname:str, pword:str):
+    def add_user(self, uname:str, pword:str, email:str):
         u_exist = 1
         uname = uname.strip()
         with open("users.json") as file:
@@ -42,21 +45,34 @@ class SignUpScreen(Screen):
             except json.decoder.JSONDecodeError:
                 users = {}
             
-        for v in users:
+        for v,v2 in users.items():
             if uname in v:
                 u_exist = 0
                 break
-        if u_exist and len(uname):
+            if v2["email"] == email:
+                u_exist = 0
+                break
+        if u_exist and len(uname) and len(email):
             users.setdefault(uname,
             {"name":uname,
             "password":sha256(pword.encode()).hexdigest(),
+            "email": email, 
             "created": datetime.now().strftime("%d-%m-%Y %H:%M")})
+            self.ids.username.text = ""
+            self.ids.password.text = ""
+            self.ids.email.text = ""
             self.manager.current = "sign_up_screen_success"
         else:
             logging.critical("DANY UZYTKOWNIK ISTNIEJE")
         with open("users.json","w") as file:
             json.dump(users,file)
-        
+    
+    def back_to_login(self):
+        self.ids.username.text = ""
+        self.ids.password.text = ""
+        self.ids.email.text = ""
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
         
 class SignUpScreenSuccess(Screen):
     def go_back(self):
@@ -73,9 +89,16 @@ class LoginScreenSuccess(Screen):
         feel = feel.lower()
         feelings = [x[:-4] for x in listdir("./quotes")]
         if feel in feelings:
+            
             with open(f"./quotes/{feel}.txt") as file:
-                quotes = [x.strip() for x in file.readlines()]
-            self.ids.quote.text = quotes[randint(0,6)] 
+                quotes = file.readlines()
+            oldQuote = self.ids.quote.text
+            newQuote =  choice(quotes)
+            
+            while oldQuote == newQuote:
+                newQuote = choice(quotes)
+                
+            self.ids.quote.text = newQuote
         else:
             self.ids.quote.text = "Please try another emotion"
         
@@ -83,8 +106,8 @@ class LoginScreenSuccess(Screen):
 class RootWidget(ScreenManager):
     pass
 
-
-
+class ImageButton(ButtonBehavior,HoverBehavior, Image):
+    pass
 class MainApp(App):
     def build(self):
         return RootWidget()
